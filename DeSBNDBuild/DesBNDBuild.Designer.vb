a413a9251fcca7dccc96be5ffb6166ea88f93478
Wulf2k <wulf2k@gmail.com>
2015-09-29 02:06:54 -0500

    DeSBNDBuild

---
+++ DeSBNDBuild/DesBNDBuild.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DesBNDBuild
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtBNDfile = New System.Windows.Forms.TextBox()
+        Me.lblGAFile = New System.Windows.Forms.Label()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.btnRebuild = New System.Windows.Forms.Button()
+        Me.btnExtract = New System.Windows.Forms.Button()
+        Me.txtInfo = New System.Windows.Forms.TextBox()
+        Me.btnCompress = New System.Windows.Forms.Button()
+        Me.btnDecompress = New System.Windows.Forms.Button()
+        Me.btnBackup = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtBNDfile
+        '
+        Me.txtBNDfile.AllowDrop = True
+        Me.txtBNDfile.Location = New System.Drawing.Point(44, 6)
+        Me.txtBNDfile.Name = "txtBNDfile"
+        Me.txtBNDfile.Size = New System.Drawing.Size(440, 20)
+        Me.txtBNDfile.TabIndex = 26
+        '
+        'lblGAFile
+        '
+        Me.lblGAFile.AutoSize = True
+        Me.lblGAFile.Location = New System.Drawing.Point(12, 9)
+        Me.lblGAFile.Name = "lblGAFile"
+        Me.lblGAFile.Size = New System.Drawing.Size(26, 13)
+        Me.lblGAFile.TabIndex = 28
+        Me.lblGAFile.Text = "File:"
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(487, 4)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 27
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'btnRebuild
+        '
+        Me.btnRebuild.Location = New System.Drawing.Point(487, 33)
+        Me.btnRebuild.Name = "btnRebuild"
+        Me.btnRebuild.Size = New System.Drawing.Size(75, 23)
+        Me.btnRebuild.TabIndex = 30
+        Me.btnRebuild.Text = "Rebuild"
+        Me.btnRebuild.UseVisualStyleBackColor = True
+        '
+        'btnExtract
+        '
+        Me.btnExtract.Location = New System.Drawing.Point(409, 33)
+        Me.btnExtract.Name = "btnExtract"
+        Me.btnExtract.Size = New System.Drawing.Size(75, 23)
+        Me.btnExtract.TabIndex = 29
+        Me.btnExtract.Text = "Extract"
+        Me.btnExtract.UseVisualStyleBackColor = True
+        '
+        'txtInfo
+        '
+        Me.txtInfo.Location = New System.Drawing.Point(12, 62)
+        Me.txtInfo.Multiline = True
+        Me.txtInfo.Name = "txtInfo"
+        Me.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtInfo.Size = New System.Drawing.Size(548, 142)
+        Me.txtInfo.TabIndex = 31
+        '
+        'btnCompress
+        '
+        Me.btnCompress.Enabled = False
+        Me.btnCompress.Location = New System.Drawing.Point(92, 33)
+        Me.btnCompress.Name = "btnCompress"
+        Me.btnCompress.Size = New System.Drawing.Size(75, 23)
+        Me.btnCompress.TabIndex = 33
+        Me.btnCompress.Text = "Compress"
+        Me.btnCompress.UseVisualStyleBackColor = True
+        '
+        'btnDecompress
+        '
+        Me.btnDecompress.Enabled = False
+        Me.btnDecompress.Location = New System.Drawing.Point(14, 33)
+        Me.btnDecompress.Name = "btnDecompress"
+        Me.btnDecompress.Size = New System.Drawing.Size(75, 23)
+        Me.btnDecompress.TabIndex = 32
+        Me.btnDecompress.Text = "Decompress"
+        Me.btnDecompress.UseVisualStyleBackColor = True
+        '
+        'btnBackup
+        '
+        Me.btnBackup.Location = New System.Drawing.Point(288, 33)
+        Me.btnBackup.Name = "btnBackup"
+        Me.btnBackup.Size = New System.Drawing.Size(75, 23)
+        Me.btnBackup.TabIndex = 34
+        Me.btnBackup.Text = "Backup"
+        Me.btnBackup.UseVisualStyleBackColor = True
+        '
+        'DesBNDBuild
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(572, 216)
+        Me.Controls.Add(Me.btnBackup)
+        Me.Controls.Add(Me.btnCompress)
+        Me.Controls.Add(Me.btnDecompress)
+        Me.Controls.Add(Me.txtInfo)
+        Me.Controls.Add(Me.btnRebuild)
+        Me.Controls.Add(Me.btnExtract)
+        Me.Controls.Add(Me.txtBNDfile)
+        Me.Controls.Add(Me.lblGAFile)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Name = "DesBNDBuild"
+        Me.Text = "Wulf's DeS BND Rebuilder 0.700"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtBNDfile As System.Windows.Forms.TextBox
+    Friend WithEvents lblGAFile As System.Windows.Forms.Label
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents btnRebuild As System.Windows.Forms.Button
+    Friend WithEvents btnExtract As System.Windows.Forms.Button
+    Friend WithEvents txtInfo As System.Windows.Forms.TextBox
+    Friend WithEvents btnCompress As System.Windows.Forms.Button
+    Friend WithEvents btnDecompress As System.Windows.Forms.Button
+    Friend WithEvents btnBackup As System.Windows.Forms.Button
+
+End Class
---
+++ DeSBNDBuild/DesBNDBuild.vb
@@ -0,0 +1,234 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+
+
+Public Class DesBNDBuild
+    Public Shared bytes() As Byte
+    Public Shared filename As String
+    Public Shared filepath As String
+
+    Public Shared extractPath As String
+
+    Public Shared BinderID As String
+    Public Shared flags As UInteger
+    Public Shared numFiles As UInteger
+    Public Shared namesEndLoc As UInteger
+
+
+    Private Function StrFromBytes(ByVal loc As UInteger) As String
+        Dim Str As String = ""
+        Dim cont As Boolean = True
+
+        While cont
+            If bytes(loc) > 0 Then
+                Str = Str + Convert.ToChar(bytes(loc))
+                loc += 1
+            Else
+                cont = False
+            End If
+        End While
+
+        Return Str
+    End Function
+    Private Function UIntFromBytes(ByVal loc As UInteger) As UInteger
+        Dim tmpUint As UInteger = 0
+
+        For i = 0 To 3
+            tmpUint += Convert.ToUInt16(bytes(loc + i)) * &H100 ^ (3 - i)
+        Next
+
+        Return tmpUint
+    End Function
+
+    Private Sub UINTToBytes(ByVal loc As UInteger, val As UInteger)
+        Dim BArr(3) As Byte
+
+        For i = 0 To 3
+            BArr(i) = Math.Floor(val / (&H100 ^ (3 - i))) Mod &H100
+        Next
+
+        Array.Copy(BArr, 0, bytes, loc, 4)
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        Dim openDlg As New OpenFileDialog()
+
+        openDlg.Filter = "DeS DCX/BND File|*BND;*DCX"
+        openDlg.Title = "Open your BND file"
+
+        If openDlg.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            txtBNDfile.Text = openDlg.FileName
+        End If
+
+
+    End Sub
+
+    Private Sub btnExtract_Click(sender As Object, e As EventArgs) Handles btnExtract.Click
+        Dim currFileSize As UInteger
+        Dim currFileOffset As UInteger
+        Dim currFileID As UInteger
+        Dim currFileNameOffset As UInteger
+        Dim currFileName As String
+        Dim currFilePath As String
+        Dim currFileBytes() As Byte = {}
+
+        filepath = Microsoft.VisualBasic.Left(txtBNDfile.Text, InStrRev(txtBNDfile.Text, "\"))
+        filename = Microsoft.VisualBasic.Right(txtBNDfile.Text, txtBNDfile.Text.Length - filepath.Length)
+
+        bytes = File.ReadAllBytes(filepath & filename)
+
+        BinderID = StrFromBytes(&H0)
+        flags = UIntFromBytes(&HC)
+        numFiles = UIntFromBytes(&H10)
+        namesEndLoc = UIntFromBytes(&H14)
+
+        For i As UInteger = 0 To numFiles - 1
+            currFileSize = UIntFromBytes(&H24 + i * &H18)
+            currFileOffset = UIntFromBytes(&H28 + i * &H18)
+            currFileID = UIntFromBytes(&H2C + i * &H18)
+            currFileNameOffset = UIntFromBytes(&H30 + i * &H18)
+
+            currFileName = StrFromBytes(currFileNameOffset)
+
+            currFileName = filepath & filename & ".extract\" & Microsoft.VisualBasic.Right(currFileName, currFileName.Length - &H1B)
+            currFilePath = Microsoft.VisualBasic.Left(currFileName, InStrRev(currFileName, "\"))
+            currFileName = Microsoft.VisualBasic.Right(currFileName, currFileName.Length - currFilePath.Length)
+
+            If (Not System.IO.Directory.Exists(currFilePath)) Then
+                System.IO.Directory.CreateDirectory(currFilePath)
+            End If
+
+
+            ReDim currFileBytes(currFileSize - 1)
+            Array.Copy(bytes, currFileOffset, currFileBytes, 0, currFileSize)
+            File.WriteAllBytes(currFilePath & currFileName, currFileBytes)
+
+
+
+        Next
+        txtInfo.Text += TimeOfDay & " - Extracted." & Environment.NewLine
+    End Sub
+    Private Sub btnRebuild_Click(sender As Object, e As EventArgs) Handles btnRebuild.Click
+        Dim currFileSize As UInteger
+        Dim currFileOffset As UInteger
+        Dim currFileNameOffset As UInteger
+        Dim currFileName As String
+        Dim currFilePath As String
+        Dim currFileBytes() As Byte = {}
+
+        Dim padding As UInteger
+
+        filepath = Microsoft.VisualBasic.Left(txtBNDfile.Text, InStrRev(txtBNDfile.Text, "\"))
+        filename = Microsoft.VisualBasic.Right(txtBNDfile.Text, txtBNDfile.Text.Length - filepath.Length)
+
+        bytes = File.ReadAllBytes(filepath & filename)
+
+        BinderID = StrFromBytes(&H0)
+        flags = UIntFromBytes(&HC)
+        numFiles = UIntFromBytes(&H10)
+        namesEndLoc = UIntFromBytes(&H14)
+
+        If namesEndLoc Mod &H10 > 0 Then
+            padding = &H10 - (namesEndLoc Mod &H10)
+        Else
+            padding = 0
+        End If
+
+
+        ReDim Preserve bytes(namesEndLoc + padding - 1)
+
+
+        For i As UInteger = 0 To numFiles - 1
+            currFileNameOffset = UIntFromBytes(&H30 + i * &H18)
+
+            currFileName = StrFromBytes(currFileNameOffset)
+            currFileName = filepath & filename & ".extract\" & Microsoft.VisualBasic.Right(currFileName, currFileName.Length - &H1B)
+            currFilePath = Microsoft.VisualBasic.Left(currFileName, InStrRev(currFileName, "\"))
+            currFileName = Microsoft.VisualBasic.Right(currFileName, currFileName.Length - currFilePath.Length)
+
+            currFileBytes = File.ReadAllBytes(currFilePath & currFileName)
+            currFileSize = currFileBytes.Length
+
+            If currFileSize Mod &H10 > 0 And i < numFiles - 1 Then
+                padding = &H10 - (currFileSize Mod &H10)
+            Else
+                padding = 0
+            End If
+
+            currFileOffset = bytes.Length
+            UINTToBytes(&H24 + i * &H18, currFileSize)
+            UINTToBytes(&H28 + i * &H18, currFileOffset)
+            UINTToBytes(&H34 + i * &H18, currFileSize)
+
+            ReDim Preserve bytes(bytes.Length + currFileSize + padding - 1)
+            Array.Copy(currFileBytes, 0, bytes, currFileOffset, currFileSize)
+        Next
+        File.WriteAllBytes(filepath & filename, bytes)
+        txtInfo.Text += TimeOfDay & " - Rebuilt." & Environment.NewLine
+    End Sub
+    Public Function Decompress(ByVal cmpBytes() As Byte) As Byte()
+        Dim sourceFile As MemoryStream = New MemoryStream(cmpBytes)
+        Dim destFile As MemoryStream = New MemoryStream()
+        Dim compStream As New DeflateStream(sourceFile, CompressionMode.Decompress)
+        Dim myByte As Integer = compStream.ReadByte()
+
+        While myByte <> -1
+            destFile.WriteByte(CType(myByte, Byte))
+            myByte = compStream.ReadByte()
+        End While
+
+        destFile.Close()
+        sourceFile.Close()
+
+        Return destFile.ToArray
+    End Function
+    Private Sub btnDecompress_Click(sender As Object, e As EventArgs) Handles btnDecompress.Click
+        filepath = Microsoft.VisualBasic.Left(txtBNDfile.Text, InStrRev(txtBNDfile.Text, "\"))
+        filename = Microsoft.VisualBasic.Right(txtBNDfile.Text, txtBNDfile.Text.Length - filepath.Length)
+
+        bytes = File.ReadAllBytes(filepath & filename)
+
+
+        Dim newbytes(&H44D9) As Byte
+        Dim decbytes(&H10000) As Byte
+
+        Array.Copy(bytes, &H120, newbytes, 0, &H44D9)
+
+        decbytes = Decompress(newbytes)
+
+
+    End Sub
+    Private Sub btnCompress_Click(sender As Object, e As EventArgs) Handles btnCompress.Click
+        filepath = Microsoft.VisualBasic.Left(txtBNDfile.Text, InStrRev(txtBNDfile.Text, "\"))
+        filename = Microsoft.VisualBasic.Right(txtBNDfile.Text, txtBNDfile.Text.Length - filepath.Length)
+
+        bytes = File.ReadAllBytes(filepath & filename)
+        Dim newbytes() As Byte
+
+
+        Dim stream = New MemoryStream()
+        Dim zipStream = New DeflateStream(stream, CompressionLevel.Optimal)
+        zipStream.Write(bytes, 0, 65536)
+        zipStream.Close()
+
+        newbytes = stream.ToArray()
+
+
+
+
+        File.WriteAllBytes(filepath & filename & ".test", newbytes)
+    End Sub
+
+    Private Sub txt_Drop(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles txtBNDfile.DragDrop
+        Dim file() As String = e.Data.GetData(DataFormats.FileDrop)
+        sender.Text = file(0)
+    End Sub
+    Private Sub txt_DragEnter(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles txtBNDfile.DragEnter
+        e.Effect = DragDropEffects.Copy
+    End Sub
+
+
+    Private Sub btnBackup_Click(sender As Object, e As EventArgs) Handles btnBackup.Click
+
+    End Sub
+End Class
