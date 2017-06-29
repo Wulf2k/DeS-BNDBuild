f8d86313916c3926a912ea1888bb02210b41d6fb
Wulf2k <wulf2k@gmail.com>
2017-06-28 20:44:58 -0500

    Updated label for compiled EXE release.

--- DeS-BNDBuild/DeS_BNDBuild.Designer.vb
+++ DeS-BNDBuild/DeS_BNDBuild.Designer.vb
@@ -100,7 +100,7 @@ Partial Class Des_BNDBuild
         Me.lblVersion.Name = "lblVersion"
         Me.lblVersion.Size = New System.Drawing.Size(76, 13)
         Me.lblVersion.TabIndex = 42
-        Me.lblVersion.Text = "2017-05-11-13"
+        Me.lblVersion.Text = "2017-06-28-20"
         '
         'Des_BNDBuild
         '
